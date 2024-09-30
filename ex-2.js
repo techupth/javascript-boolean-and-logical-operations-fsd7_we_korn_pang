d9a44cab190e77007c4c273b9a466c713e9664d0
pangphaichit <pg_grj@hotmail.com>
2024-09-30 15:38:01 +0700

    ex4-boolean and logical operation

--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,21 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
+let lastMonthPaidMoreThan4000 = true;
+let isWeekday = true;
+let hasBoughtProductFromITCategory = true;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = true;
 
-let hasPromotion;
+
+
+
+lastMonthPaidMoreThan4000 = true;
+isWeekday = true;
+hasBoughtProductFromITCategory = false;
+hasAttendedDiscountEvent = true;
+isPlatinum = false;
+
+let hasPromotion = (lastMonthPaidMoreThan4000 && isWeekday && !hasBoughtProductFromITCategory && !hasAttendedDiscountEvent)  || isPlatinum;
 
 console.log(hasPromotion);
